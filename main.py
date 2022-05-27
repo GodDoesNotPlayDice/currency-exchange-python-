8eba3b689dafce6f6bffd859bb0469b848b03233
Doze <69562683+kuroroAsync@users.noreply.github.com>
2022-05-27 17:44:15 -0400

    27/05/2022

---
+++ main.py
@@ -0,0 +1,135 @@
+import os
+from src.functions import *
+paises = currencysFormatter(country,0)
+paises_cod = currencysFormatter(country,1)
+cod_value = currencysFormatter(country,2)
+time = fecha_hoy()
+fecha = lambda x : f'{time[2]}/{time[1]}/{time[0]}'
+hora = lambda x : f'{time[3]}:{time[4]}'
+close = True
+date,result,estado = '','',''
+conversiones = []
+while close:
+    try:
+        os.system('cls')
+        print('#################')
+        print(f'Datos...\nPaises Consultados: {date}\nUltima conversion: {result}')
+        print(f'Estado de Operacion: {estado}')
+        print('#################')
+        print("Bienvenido al conversor online (Python)")
+        print(f"Fecha de hoy {fecha(time)}, Hora de hoy {hora(time)}.")
+        print("1) Buscar codigo de divisa (Busqueda por pais)")
+        print("2) Hacer conversion de dos monedas (Comparar dos Monedas, con codigo de divisa)")
+        print("3) Ver valores actuales de las monedas (Comparadas con USD, EUR, GBP, JPY, CNY, RUB, INR)")
+        print("4) Salir")
+        opc = int(input("Porfavor oprime una opcion: "))
+        if opc == 1:
+            cont = 1
+            print("Listas de Paises...")
+            for i in paises:
+                print(f"{cont}) {i.strip()}")
+                cont+=1
+            while True:
+                pais = int(input("Numero de Pais: "))-1
+                validPais = True if not pais < 0 or pais > 268 else False
+                date = validCountry(validPais,pais)
+                if validPais:
+                    estado = 'Consulta de Pais terminado..'
+                    break
+        elif opc == 2:
+            print("Usar codigo de divisas")
+            while True:
+                try:
+                    from_ = str(input('Pais de: ')).upper()
+                    to_ = str(input('Destino de: ')).upper()
+                    if len(from_) > 3 and len(to_) > 3:
+                        estado = "Datos Invalidos, recuerda que el codigo de divisa es de 3 DIGITOS"
+                        break
+                    else:
+                        if not cod_value.count(from_) and cod_value.count(to_):
+                            estado = "Un codigo de pais no es valido"
+                            break
+                        else:
+                            value_ = currency_comparing(from_,to_)
+                            indexTo_ = cod_value.index(to_)
+                            indexFrom_ = cod_value.index(from_)
+                            fecha_valor = fechaCurrency(from_,to_)
+                            print(f"Actualizacion de los valores: {fecha_valor}")
+                            valorActual = f' 1 {from_} = {value_} {to_}'
+                            print(f"Valor actual: {valorActual}")
+                            while True:
+                                try:
+                                    cantidad = float(input('Cantidad que desea convertir: '))
+                                    op = cantidad * float(value_)
+                                    result = f'{round(float((op)),2)} {paises_cod[indexTo_]}'
+                                    print(f"Cantidad convetida: {result}")
+                                    conversiones.append([historial(indexFrom_,indexTo_,valorActual,op,fecha(time),hora(time),fecha_valor)])
+                                    break
+                                except ValueError:
+                                    print('CANTIDAD VALOR --porfavor ingresar un valor valido')
+                            opc_aux = str(input('Desea otra operacion?\n1)Si\nX)No \ (X)Representa cualquier tecla\nOpcion: '))
+                            if not opc_aux == '1':
+                                estado = 'Operacion de Conversion terminada.'
+                                break
+                except ValueError:
+                     estado = 'Codigos de paises no econtrados'
+                     break
+        elif opc == 3:
+            while True:
+                try:
+                    print('USD', 'EUR', 'GBP', 'JPY', 'CNY', 'RUB', 'INR')
+                    from_ = str(input('Codigo de pais de: ')).upper()
+                    if len(from_) > 3:
+                        estado = "Datos Invalidos, recuerda que el codigo de divisa es de 3 DIGITOS"
+                        break
+                    else:
+                        if not cod_value.count(from_):
+                            estado = "Un codigo de pais no es valido"
+                            break
+                        else:
+                            while True:
+                                try:
+                                    value_ = float(input(f'Cantidad de {from_}: '))
+                                    print('Cargando datos...')
+                                    big_currencys = ['USD', 'EUR', 'GBP', 'JPY', 'CNY', 'RUB', 'INR']
+                                    g_values = []
+                                    for i in big_currencys:
+                                        v = value_ * float(currency_comparing(from_,i))
+                                        g_values.append(f'Valor de {value_} {from_} es {round(v,2)} {i}')
+                                    for i in g_values:
+                                        print(i)
+                                    break
+                                except ValueError:
+                                    print('Ingresa un codigo valido')
+                            opc_aux = str(input('Desea otra operacion?\n1)Si\nX)No \ (X)Representa cualquier tecla\nOpcion: '))
+                            if not opc_aux == '1':
+                                estado = 'Consulta de valores terminada...'
+                                break
+                except ValueError:
+                    estado = "Un codigo de pais no es valido o no es encontrado"
+                    break
+        elif opc == 4:
+            close = False
+            os.system('cls')
+            print("Hasta pronto!...")
+            t.sleep(1)
+            cont = 3
+            while not cont == 0 :
+                os.system('cls')
+                print(f"la aplicacion se cerrara en {cont} Segundos")
+                cont-=1
+                t.sleep(1)
+            quit()
+        else:
+            estado = "Opcion invlida"
+    except ValueError:
+        estado = "VALUE ERROR (MENU - MAIN ) ingrese nuevamente un dato valido "
+    except IndexError:
+        estado = " (INDEX ERROR - MAIN ) ingrese nuevamente un dato valido "
+    
+
+
+
+
+
+    
--- src/functions.py
+++ src/functions.py
@@ -1,9 +1,12 @@
+import re
+import pandas as pd
 from bs4 import BeautifulSoup
-from pytz import country_names
 import requests as rq
-root = "https://www.xe.com/es/currencyconverter/convert/"
+import time as t
+print("Cargando data...")
+root = "https://www.xe.com/es/currencyconverter/convert"
 flags_root = "https://www.iban.com"
-def flags():
+def countrys():
     web = f"{flags_root}/currency-codes"
     get = rq.get(web)
     content = get.text
@@ -11,20 +14,96 @@ def flags():
     bigTable = soup.find("table",class_="table table-bordered downloads tablesorter")
     table = bigTable.find("tbody")
     country = table.find_all("td")
-    flgs,flg = [],[]
+    countrys,aux = [],[]
     cont = 0
     for i in country:
         if cont == 3:
             cont = 0
-            flgs.append(flg)
-            flg = []
+            countrys.append(aux)
+            aux= []
         else:
-            flg.append(i.text)
+            aux.append(i.text)
             cont+=1
+    return countrys
+def currencysFormatter(countrys,value):
+    arr = []
+    for i in countrys:
+        arr.append(str(i[value]).strip())
+    arr = list(filter(('').__ne__,arr))
+    return arr
+def fechaCurrency(flag1,flag2):
+        web = f'{root}/?Amount=10&From={flag1}&To={flag2}'
+        get = rq.get(web)
+        content = get.text
+        soup = BeautifulSoup(content, 'lxml')
+        div = soup.find('div',class_='result__LiveSubText-sc-1bsijpp-2 iKYXwX')
+        date_time = div.text
+        return date_time
 
-    print(flgs)
-    #countrys = str(country).split(",")
-    #for i in countrys:
-    #    print(i)
+def fecha_hoy():
+    fecha = t.localtime()
+    arr = []
+    for i in fecha:
+        arr.append(str(i))
+    return arr
 
-flags()
+def currency_comparing(flag1,flag2):
+    try:
+        arr = []
+        web = f'{root}/?Amount=10&From={flag1}&To={flag2}'
+        get = rq.get(web)
+        content = get.text
+        soup = BeautifulSoup(content, 'lxml')
+        div = soup.find('div',class_='unit-rates___StyledDiv-sc-1dk593y-0 dEqdnx')
+        value = div.find('p')
+        for i in value.text.split(' ')[3]:
+            arr.append(i)
+        value = ''.join(arr)
+        value = value.replace(',','.')
+        return value
+    except AttributeError:
+        return 'Uno de los codigos no pudo ser contrado'
+
+def currencys(curr):
+    indexs = []
+    arr = []
+    cont = 0
+    for i in curr:
+        if i[1] == 'No universal currency':
+            arr.append(i)
+            indexs.append(cont)
+        cont+=1
+    curr[8][-1] = 'XCD'
+    curr[182][-1] = 'ILS'
+    curr[221][-1] = 'GBP'
+    return arr,curr
+noUniversalCountrys,country = currencys(countrys())
+def countrys_table(countrys,divisa,div_code):
+    dict_all = {f"Paises" : countrys, "Divisa": divisa, "Codigo" : div_code}
+    data_frame = pd.DataFrame.from_dict(dict_all)
+    return data_frame
+tablaDivisas = countrys_table(currencysFormatter(country,0),currencysFormatter(country,1),currencysFormatter(country,2))
+tablaDivisas.to_csv('src/Divisas.csv')
+
+# Respaldo DIVISAS (csv)
+
+def validCountry(valid,index):
+    if valid:
+        nombre = currencysFormatter(country,0)[index]
+        divisa = currencysFormatter(country,1)[index]
+        codigo_div = currencysFormatter(country,2)[index]
+        return f'Nombre: {nombre.capitalize()} | Divisa: {divisa.capitalize()} | Codigo_divisa: {codigo_div}'
+    else:
+        return f'(VALID COUNTRY) --Pais no encontrado!'
+
+def historial(from_,to_,valueActual,value,date_time,time,value_time):
+    try:
+        return f'Pais de: {currencysFormatter(country,0)[from_]} A Pais de destino: {currencysFormatter(country,0)[to_]},\n Valor Actual: {valueActual}, Valor converitido: {value}\n Actualizacion del valor: {value_time}\n Fecha y Hora de la conversion: {date_time}, {time}'
+    except IndexError:
+        return 'INDEX ERROR'
+
+def allcurrencysFormatter(flag1):
+    pass
+
+def value(value,cant):
+    pass
